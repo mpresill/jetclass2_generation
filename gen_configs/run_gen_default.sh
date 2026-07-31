67a28d120dccf989c1d0bfce8bb29664c6ebdefa
mpresill <26869915+mpresill@users.noreply.github.com>
2026-07-31 18:03:05 +0200

    Add madspin card template copy to run script
    Added functionality to copy madspin card template if it exists.

--- gen_configs/run_gen_default.sh
+++ gen_configs/run_gen_default.sh
@@ -42,6 +42,11 @@ if [ -f mg5_step2_run_card_templ.dat ]; then
     cp -f mg5_step2_run_card_templ.dat $MDIR/Cards/run_card.dat
 fi
 
+# if mg5_step2_madspin_card_templ exists, copy it to the MG dir
+if [ -f mg5_step2_madspin_card_templ.dat ]; then
+    cp -f mg5_step2_madspin_card_templ.dat $MDIR/Cards/madspin_card.dat
+fi
+
 ## generate MG events
 rm -rf $MDIR/Events/*
 cat mg5_step2.dat | $MDIR/bin/generate_events pilotrun
